2f550db1f42776054d57b05b462f38a7f3ba4c69
Martin Plieske <martin.plieske@gmail.com>
2021-01-17 19:39:28 +0100

    implement basic Controller

--- src/main/java/de/mplieske/projektwoche/controller/Controller.java
+++ src/main/java/de/mplieske/projektwoche/controller/Controller.java
@@ -1,4 +1,16 @@
 package de.mplieske.projektwoche.controller;
 
+import de.mplieske.projektwoche.model.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Controller {
+
+   private static final Logger LOGGER = LoggerFactory.getLogger(Controller.class);
+   private static final Model MODEL = new Model();
+
+   public Model getModel() {
+      return MODEL;
+   }
+
 }
