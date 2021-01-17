177535d374b4ba62230f0a0246573ddc1e974ab6
Martin Plieske <martin.plieske@gmail.com>
2021-01-17 19:45:13 +0100

    add interface for view controllers

--- src/main/java/de/mplieske/projektwoche/view/ViewController.java
+++ src/main/java/de/mplieske/projektwoche/view/ViewController.java
@@ -1,5 +1,11 @@
 package de.mplieske.projektwoche.view;
 
+import de.mplieske.projektwoche.controller.Controller;
+
 public interface ViewController {
+
+   void setController(Controller controller);
+
+   void dataInit();
 
 }
